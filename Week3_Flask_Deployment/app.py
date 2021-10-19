fcec74586c94db5040539bc61a0f06762368c628
teddywaweru <teddywaweru@gmail.com>
2021-10-19 13:44:43 +0300

    Model to pkl & Pylint errors fixes

--- Week3_Flask_Deployment/app.py
+++ Week3_Flask_Deployment/app.py
@@ -1,17 +1,35 @@
+"""Landing page for Flask Deployment app"""
+
+
+import pickle
 from flask import Flask
 from flask import render_template
+# from flask.wrappers import Request
 
 app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
+# prediction_model = None
+
+
+class LoadModel(object):
+    """Loading model Function"""
+    def __init__(self):
+        self.prediction_model = None
+
+    def load_model(self):
+        '''Loading Model'''
+        self.prediction_model = pickle.load(open('model.pkl','rb'))
+        return self.prediction_model
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    """Landing Page loading"""
+    return render_template('index.html')
 
-@app.route('/second_page')
-def second_page():
+@app.route('/get_prediction', methods = ['POST'])
+def get_prediction():
+    """Landing Page loading"""
     return render_template('secondpage.html')
-   
 
 
 if __name__ == '__main__':
